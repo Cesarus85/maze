6bcf00ece04de8187ae629feecc366b24ac691f4
Cesarus85 <145186748+Cesarus85@users.noreply.github.com>
2025-09-08 21:59:11 +0200

    Delete src/openaiClient.js

--- src/openaiClient.js
+++
@@ -1,5 +0,0 @@
-export async function fetchMazeFromBackend(params) {
-  // Später: fetch('/api/maze', { method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify(params) })
-  // Hier: Stub -> null, damit main.js lokal generiert
-  return null;
-}
